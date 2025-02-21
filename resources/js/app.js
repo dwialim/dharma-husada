7210f967354f043d6afbdb2046041b454f200c20
dwialim <dweialim@gmail.com>
2025-02-21 23:03:20 +0700

    add tailwind css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,1 @@
-import './bootstrap';
+import '@js/bootstrap'
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,11 +2,18 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>Document</title>
+	@vite('resources/css/app.css')
+
 </head>
 <body>
+	<h1 class="text-3xl text-red-500 font-bold underline">
+		Hello world!
+	</h1>
+
+	<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+	@vite('resources/js/app.js')
 
 </body>
 </html>
